3ac9902ff7556da51f4b32bd0ddd02251b0eee69
John O'Hara <johnoharaa@gmail.com>
2024-05-06 11:40:48 -0700

    Got basic proof of concept running

--- piController.js
+++ piController.js
@@ -12,5 +12,36 @@ import piConfig from "./pi-conf.js";
  * @param {number} piId the id of the pi in the array defined in pi-conf.js. 
  */
 export function connectAndReboot(piId) {
+  // TODO: Should this be a string for the host? Is there a better way? 
+  //  Unique indices in the config array?
+  console.log(`connectAndReboot: received piId of '${piId}'`);
 
+  const sshConnection = new Client();
+
+  // hardcoded mez screen config
+  const connectionConfig = {
+    // host: 'mez-bar1',
+    host: 'togo2',
+    port: 22,
+    username: 'pi',
+    // password: 'voivod'
+    // password: 'voi42vod'
+    password: 'voivod'
+  };
+
+  sshConnection.on('ready', () => {
+    console.log(`SSH Connection to '${connectionConfig.host}' established.`);
+    // restart logic here
+
+    sshConnection.exec('sudo reboot', (err, stream) => {
+      // TODO: Maybe use stream for something here? 
+
+      if (err) {
+        console.error(`Failed to reboot '${connectionConfig.host}':`, err);
+      }
+
+      sshConnection.end();
+    });
+    // close ssh connection
+  }).connect(connectionConfig);
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,25 @@
 import express from "express";
+import { Client } from "ssh2";
+
+import { connectAndReboot } from "../piController.js";
+
 const router = express.Router();
 
 // API routes
 router.get('/restart/:screenId', (req, res) => {
   const { screenId } = req.params;
+
+  // hardcoded right coffee screen test
+  if (screenId === '420') {
+    // console.log("Testing Right togo/coffee screen restart...");
+    console.log("Testing left mez screen restart...");
+    // res.send("Route /api/restart/420 triggering reboot of left mez screen...");
+    res.send("Route /api/restart/420 triggering reboot of right coffee screen...");
+
+    connectAndReboot(420);
+
+    return;
+  }
 
   // TODO: Where do import my array of screens from? Should I have screens.js or something
   //  similar, and then manually input the IPs?

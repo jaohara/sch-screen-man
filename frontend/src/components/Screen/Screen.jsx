aaab64cd63493e9b247ca29a257336c7efdca5ba
John O'Hara <johnoharaa@gmail.com>
2024-05-07 17:43:32 -0700

    Began blocking out frontend ui

---
+++ frontend/src/components/Screen/Screen.jsx
@@ -0,0 +1,26 @@
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+
+import styles from "./Screen.module.scss";
+
+function Screen ({
+
+}) {
+  
+
+  // on initial load
+  useEffect(() => {
+    // ping screen to get status
+    // 
+  }, []);
+
+  return (
+    <div className={styles.screen}>
+
+    </div>
+  )
+}
+
+export default Screen;
---
+++ frontend/src/components/ScreenGroup/ScreenGroup.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from "./ScreenGroup.module.scss";
+
+function ScreenGroup ({
+
+}) {
+  return (
+    <div className={styles.group}>
+
+    </div>
+  ) 
+}
+
+export default ScreenGroup;

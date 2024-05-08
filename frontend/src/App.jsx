081aad477f5d7ebb64081bf18f8da053f5a6d3df
John O'Hara <johnoharaa@gmail.com>
2024-05-07 17:43:50 -0700

    Further UI scaffolding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import styles from './App.module.scss';
 
+import ScreenGroup from './components/ScreenGroup/ScreenGroup';
+
 function App() {
   // const [count, setCount] = useState(0);
 
@@ -8,12 +10,21 @@ function App() {
     - load pi-conf.js
     - parse screen config object into groups of screens
     - pass each group of screens into a ScreenGroup component
-
   */
 
 
   return (
     <div className={styles.app}>
+      <div className={styles.header}>
+        <h1>Stoup Screen Manager</h1>
+      </div>
+
+      {/* 
+        Make a screen group container div where we map each screen group data object
+        to a ScreenGroup.
+
+        The app screen should be static, and just this screen group container 
+      */}
 
     </div>
   );
--- frontend/src/components/ScreenGroup/ScreenGroup.jsx
+++ frontend/src/components/ScreenGroup/ScreenGroup.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import styles from "./ScreenGroup.module.scss";
 
+import Screen from '../Screen/Screen';
+
 function ScreenGroup ({
 
 }) {
   return (
     <div className={styles.group}>
+
 
     </div>
   ) 
